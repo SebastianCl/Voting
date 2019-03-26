6dd40c5ea0656f8a35e468d85bed5c25aacf5d11
Sebastian <cardonaloaizasebastian112@gmail.com>
2019-03-25 20:45:39 -0500

    Add API Events

---
+++ Voting.Web/Controllers/API/EventsController.cs
@@ -0,0 +1,23 @@
+﻿namespace Voting.Web.Controllers.API
+{
+
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class EventsController : Controller
+    {
+        private readonly IEventRepository eventRepository;
+
+        public EventsController(IEventRepository eventRepository)
+        {
+            this.eventRepository = eventRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetEvents()
+        {
+            return this.Ok(this.eventRepository.GetAll());
+        }
+    }
+}
