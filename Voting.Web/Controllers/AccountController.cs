15e07cc9be058790f7b847b6c35e9d8b4085698a
Sebastian Cardona Loaiza <cardonaloaizasebastian112@gmail.com>
2019-04-07 20:21:14 -0500

    Edit user and change password

--- Voting.Web/Controllers/AccountController.cs
+++ Voting.Web/Controllers/AccountController.cs
@@ -74,7 +74,7 @@
                         Gender = model.Gender,
                         Birthdate = model.Birthdate,
                         CityId = model.CityId,
-                        Email = model.Username,                        
+                        Email = model.Username,
                         UserName = model.Username
                     };
                     var result = await this.userHelper.AddUserAsync(user, model.Password);
@@ -102,6 +102,87 @@
             }
             return this.View(model);
         }
+
+
+        public async Task<IActionResult> ChangeUser()
+        {
+            var user = await this.userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+            var model = new ChangeUserViewModel();
+            if (user != null)
+            {
+                model.FirstName = user.FirstName;
+                model.LastName = user.LastName;
+                model.Occupation = user.Occupation;
+                model.Stratum = user.Stratum;
+                model.Gender = user.Gender;
+                model.CityId = user.CityId;
+                model.Birthdate = user.Birthdate;
+            }
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeUser(ChangeUserViewModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var user = await this.userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+                if (user != null)
+                {
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+                    user.Occupation = model.Occupation;
+                    user.Stratum = model.Stratum;
+                    user.Gender = model.Gender;
+                    user.CityId = model.CityId;
+                    user.Birthdate = model.Birthdate;
+
+                    var respose = await this.userHelper.UpdateUserAsync(user);
+                    if (respose.Succeeded)
+                    {
+                        this.ViewBag.UserMessage = "User updated!";
+                    }
+                    else
+                    {
+                        this.ModelState.AddModelError(string.Empty, respose.Errors.FirstOrDefault().Description);
+                    }
+                }
+                else
+                {
+                    this.ModelState.AddModelError(string.Empty, "User no found.");
+                }
+            }
+            return this.View(model);
+        }        public IActionResult ChangePassword()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var user = await this.userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+                if (user != null)
+                {
+                    var result = await this.userHelper.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return this.RedirectToAction("ChangeUser");
+                    }
+                    else
+                    {
+                        this.ModelState.AddModelError(string.Empty, result.Errors.FirstOrDefault().Description);
+                    }
+                }
+                else
+                {
+                    this.ModelState.AddModelError(string.Empty, "User no found.");
+                }
+            }
+            return this.View(model);
+        }
 
 
     }
