9205ead756c1b789b41f16e47e4b5dab108f8500
Sebastian Cardona Loaiza <cardonaloaizasebastian112@gmail.com>
2019-04-18 11:21:05 -0500

    Revert "API Post Event"
    This reverts commit 6c9f308abf105ebd9dd1a187d90f045682fba065.

--- Voting.Web/Controllers/API/EventsController.cs
+++ Voting.Web/Controllers/API/EventsController.cs
@@ -4,21 +4,16 @@
     using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
-    using System.Threading.Tasks;
-    using Voting.Web.Data.Entities;
-    using Voting.Web.Helpers;
 
     [Route("api/[Controller]")]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class EventsController : Controller
     {
         private readonly IEventRepository eventRepository;
-        private readonly IUserHelper userHelper;
 
-        public EventsController(IEventRepository eventRepository, IUserHelper userHelper)
+        public EventsController(IEventRepository eventRepository)
         {
             this.eventRepository = eventRepository;
-            this.userHelper = userHelper;
         }
 
         [HttpGet]
@@ -32,33 +27,6 @@
         {
             return this.Ok(this.eventRepository.GetEventWithId(id));
         }
-
-        [HttpPost]
-        public async Task<IActionResult> PostEvent([FromBody] Common.Models.Event @event)
-        {
-            if (!ModelState.IsValid)
-            {
-                return this.BadRequest(ModelState);
-            }
-
-            var user = await this.userHelper.GetUserByEmailAsync(@event.User.Email);
-            if (user == null)
-            {
-                return this.BadRequest("Invalid user");
-            }            
-
-            var entityEvent = new Event
-            {
-                Name = @event.Name,
-                Description = @event.Description,
-                StartDate = @event.StartDate,
-                FinishDate = @event.FinishDate,
-                User = user
-            };
-            var newEvent = await this.eventRepository.CreateAsync(entityEvent);
-            return Ok(newEvent);
-        }
-
 
 
     }
