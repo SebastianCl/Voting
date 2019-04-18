5ba2b41e9c1142f17f9a10a2ea39f1e82fe24933
Sebastian Cardona Loaiza <cardonaloaizasebastian112@gmail.com>
2019-04-18 15:38:37 -0500

    Post Event

--- Voting.Web/Controllers/API/EventsController.cs
+++ Voting.Web/Controllers/API/EventsController.cs
@@ -1,6 +1,9 @@
 ﻿namespace Voting.Web.Controllers.API
 {
+    using System.Threading.Tasks;
     using Data;
+    using Data.Entities;
+    using Helpers;
     using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
@@ -10,10 +13,14 @@
     public class EventsController : Controller
     {
         private readonly IEventRepository eventRepository;
+        private readonly IUserHelper userHelper;
 
-        public EventsController(IEventRepository eventRepository)
+        public EventsController(
+            IEventRepository eventRepository,
+            IUserHelper userHelper)
         {
             this.eventRepository = eventRepository;
+            this.userHelper = userHelper;
         }
 
         [HttpGet]
@@ -27,7 +34,33 @@
         {
             return this.Ok(this.eventRepository.GetEventWithId(id));
         }
+        /*
+        [HttpPost]
+        public async Task<IActionResult> PostEvent([FromBody] Common.Models.Event @event)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest(ModelState);
+            }
 
+            var user = await this.userHelper.GetUserByEmailAsync(@event.User.Email);
+            if (user == null)
+            {
+                return this.BadRequest("Invalid user");
+            }
+
+            var entityEvent = new Event
+            {
+                Name = @event.Name,
+                Description = @event.Description,
+                StartDate = @event.StartDate,
+                FinishDate = @event.FinishDate,
+                User = user
+            };
+            var newEvent = await this.eventRepository.CreateAsync(entityEvent);
+            return Ok(newEvent);
+        }
+        */
 
     }
 }
--- Voting.Web/Controllers/API/VotesController.cs
+++ Voting.Web/Controllers/API/VotesController.cs
@@ -5,19 +5,24 @@
     using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+    using Voting.Web.Data.Entities;
 
     [Route("api/[controller]")]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class VotesController : Controller
     {
         private readonly IVoteRepository voteRepository;
+        private readonly IEventRepository eventRepository;
         private readonly IUserHelper userHelper;
 
         public VotesController(
             IVoteRepository voteRepository,
+            IEventRepository eventRepository,
             IUserHelper userHelper)
         {
             this.voteRepository = voteRepository;
+            this.eventRepository = eventRepository;
             this.userHelper = userHelper;
         }
 
@@ -39,32 +44,41 @@
             return this.Ok(this.voteRepository.GetVotesOfCandidate(candidateId));
         }*/
 
-        /*[HttpPost]
+        [HttpPost]
         public async Task<IActionResult> PostVote([FromBody] Common.Models.Vote vote)
         {
             if (!ModelState.IsValid)
             {
                 return this.BadRequest(ModelState);
             }
+
             var user = await this.userHelper.GetUserByEmailAsync(vote.User.Email);
             if (user == null)
             {
                 return this.BadRequest("Invalid user");
             }
-            //TODO: Upload images
-            var entityEvent = new Vote
+
+            var @event = await this.eventRepository.GetByIdAsync(vote.Event.Id);
+            if (@event == null)
             {
-                Event = vote.ev
-                LastPurchase = vote.LastPurchase,
-                LastSale = vote.LastSale,
-                Name = vote.Name,
-                Price = vote.Price,
-                Stock = vote.Stock,
-                User = user
+                return this.BadRequest("Invalid event");
+            }
+
+            var candidate = await this.eventRepository.GetCandidateAsync(vote.Candidate.Id);
+            if (candidate == null)
+            {
+                return this.BadRequest("Invalid candidate");
+            }
+
+            var entityVote = new Vote
+            {
+                User = user,
+                Event = @event,
+                Candidate = candidate
             };
-            var newProduct = await this.eventRepository.CreateAsync(entityVote);
-            return Ok(newProduct);
-        }*/
+            var newVote = await this.voteRepository.CreateAsync(entityVote);
+            return Ok(newVote);
+        }
 
 
 
